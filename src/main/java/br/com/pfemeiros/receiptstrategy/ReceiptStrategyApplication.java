97addee13e369c798c85bc98e6d96d9e6fa23346
Pedro Medeiros <pedro.medeiros@redspark.io>
2021-01-21 19:34:46 -0300

    first commit

---
+++ src/main/java/br/com/pfemeiros/receiptstrategy/ReceiptStrategyApplication.java
@@ -0,0 +1,13 @@
+package br.com.pfemeiros.receiptstrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pfemeiros/receiptstrategy/ReceiptStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pfemeiros.receiptstrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
