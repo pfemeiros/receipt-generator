8e84a5cf715ffea16a95aad0d7688a257afbd27f
Pedro Medeiros <pedro.medeiros@redspark.io>
2021-01-24 21:16:57 -0300

    fix layout

--- src/main/java/br/com/pfemeiros/receiptstrategy/util/PDFUtils.java
+++ src/main/java/br/com/pfemeiros/receiptstrategy/util/PDFUtils.java
@@ -1,24 +1,30 @@
 package br.com.pfemeiros.receiptstrategy.util;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
-import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
 
 public class PDFUtils {
 
     public static byte[] generateFile(String html) {
         try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ITextRenderer renderer = new ITextRenderer();
-            renderer.setDocumentFromString(html);
-            renderer.layout();
-            renderer.createPDF(baos);
+            Document document = new Document();
+            PdfWriter.getInstance(document, baos);
+            document.open();
+            HTMLWorker htmlWorker = new HTMLWorker(document);// TODO check deprecated
+            htmlWorker.parse(new StringReader(html));
+            document.close();
             baos.close();
             return baos.toByteArray();
-        } catch (IOException | com.lowagie.text.DocumentException e) {
+        } catch (DocumentException | IOException e) {
             e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not read file");
         }
